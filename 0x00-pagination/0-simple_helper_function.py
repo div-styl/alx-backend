57c74f1db98458bbbf6227ab8897520109fb105c
div-styl <gouhmada@gmail.com>
2024-01-27 12:41:32 +0100

    Refactor index_range function to use page_size consistently

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """func returns index and an end of the range"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """returns index and an end of the range"""
     return ((page - 1) * page_size, page * page_size)
