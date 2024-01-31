4b6390fd913398e6273f9dc57b0d067b1e9d6ef2
div-styl <gouhmada@gmail.com>
2024-01-31 19:55:00 +0100

    Update!

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,7 +18,7 @@ class FIFOCache(BaseCaching):
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 lkey = next(iter(self.cache_data))
                 self.cache_data.pop(lkey)
-                print("DISCARD: {}".format(lkey))
+                print("DISCARD:", key)
 
     def get(self, key):
         """get"""
