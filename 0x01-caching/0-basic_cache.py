ec1c89d7ee5d39379cef0ddacbcfc050e5db03fc
div-styl <gouhmada@gmail.com>
2024-01-31 19:31:28 +0100

    Update!

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,7 +14,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
-        if key is None and item is None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
 
