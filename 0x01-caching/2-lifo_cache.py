d880d4a561b5195a79f0712d64ca11d65ede6e43
div-styl <gouhmada@gmail.com>
2024-01-31 20:07:20 +0100

    Update!

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 """
 Create a class LIFOCache that inherits
-from BaseCaching and is a caching system:
-
-You must use self.cache_data - dictionary from the parent class BaseCaching
-You can overload def __init__(self):
-but don’t forget to call the parent init: super().__init__()
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the item value for the key key.
-If key or item is None, this method should not do anything.
-If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-you must discard the last item put in cache (LIFO algorithm)
-you must print DISCARD: with the key discarded and following by a new line
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in self.cache_data, return None.
 """
 
 
@@ -27,20 +13,18 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """init"""
         super().__init__()
+        self.last = 0
 
     def put(self, key, item):
         """adds an item to cache"""
-        if key is None or item is None:
-            pass
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys():
-                lkey, last_val = self.cache.popitem()
-                print("DISCARD: {}".format(lkey))
-        self.cache_data[key] = item
+        if key and item:
+            if self.get(key) != item:
+                self.cache_data[key] = item
+                if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                    self.cache_data.pop(self.last)
+                    print('DISCARD:', self.last)
+                self.last = key
 
     def get(self, key):
         """return and item from the cache"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
