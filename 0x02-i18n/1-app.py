647a14881c7652ce4f7a99b63499f03b58cdb136
gouhmada <gouhmada@gmail.com>
2024-02-06 18:17:03 +0000

    babel is put into use

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+"""Basic route in flask app"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+class Config(object):
+    """class for babel config for languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """render the 0-index.html template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
